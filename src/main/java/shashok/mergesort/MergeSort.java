7232e131f010ec6e0e5e0b9d3eb43ccecac0d8ba
Sha512N <kirianferrett@gmail.com>
2022-02-21 19:24:27 +0700

    Refactoring and change of function placement

--- src/main/java/shashok/mergesort/MergeSort.java
+++ src/main/java/shashok/mergesort/MergeSort.java
@@ -13,34 +13,9 @@ public class MergeSort<T> {
         this.casting = casting;
     }
 
-    private long calculateBufSize(Collection<File> files) {
-        return Runtime.getRuntime().freeMemory() / (files.size() + 1) * 2 / 3;
-    }
-
-    private int castLongToInt(long l) {
-        if (!(Integer.MIN_VALUE <= l && l <= Integer.MAX_VALUE)) {
-            return Integer.MAX_VALUE;
-        }
-        return (int) l;
-    }
-
     public int mergeFiles(Collection<File> files, File output) throws IOException {
-        PriorityQueue<FileElement> queue = new PriorityQueue<>(files.size(), (o1, o2) -> {
-            T elem1 = casting.cast(o1.peek());
-            T elem2 = casting.cast(o2.peek());
-            return comparator.compare(elem1, elem2);
-        });
         int bufsize = castLongToInt(calculateBufSize(files));
-        for (File f: files) {
-            FileElement felem = new FileElement(f, bufsize);
-            while (!felem.isEmpty() && casting.cast(felem.peek()) == null) {
-                felem.pop();
-            }
-            if (felem.isEmpty()) {
-                continue;
-            }
-            queue.add(felem);
-        }
+        PriorityQueue<FileElement> queue = createFileQueue(files, bufsize);
         BufferedWriter outWriter = new BufferedWriter(new FileWriter(output), bufsize);
         int rows = 0;
         try {
@@ -49,18 +24,12 @@ public class MergeSort<T> {
                 FileElement fe = queue.poll();
                 String s = fe.pop();
                 T element = casting.cast(s);
-
+                cleanFileElement(fe);
                 if (fe.isEmpty()) {
                     fe.close();
                 } else {
-                    while (!fe.isEmpty() && casting.cast(fe.peek()) == null) {
-                        fe.pop();
-                    }
-                    if (!fe.isEmpty()) {
-                        queue.add(fe);
-                    }
+                    queue.add(fe);
                 }
-
                 if (element == null
                         ||
                         (previousValue != null && this.comparator.compare(previousValue, element) > 0)) {
@@ -78,5 +47,38 @@ public class MergeSort<T> {
         return rows;
     }
 
+    private PriorityQueue<FileElement> createFileQueue(Collection<File> files, int bufsize) {
+        PriorityQueue<FileElement> queue = new PriorityQueue<>(files.size(), (o1, o2) -> {
+            T elem1 = casting.cast(o1.peek());
+            T elem2 = casting.cast(o2.peek());
+            return comparator.compare(elem1, elem2);
+        });
 
+        for (File f: files) {
+            FileElement felem = new FileElement(f, bufsize);
+            cleanFileElement(felem);
+            if (felem.isEmpty()) {
+                continue;
+            }
+            queue.add(felem);
+        }
+        return queue;
+    }
+
+    private void cleanFileElement(FileElement fe) {
+        while (!fe.isEmpty() && casting.cast(fe.peek()) == null) {
+            fe.pop();
+        }
+    }
+
+    private long calculateBufSize(Collection<File> files) {
+        return Runtime.getRuntime().freeMemory() / (files.size() + 1) * 2 / 3;
+    }
+
+    private int castLongToInt(long l) {
+        if (!(Integer.MIN_VALUE <= l && l <= Integer.MAX_VALUE)) {
+            return Integer.MAX_VALUE;
+        }
+        return (int) l;
+    }
 }
